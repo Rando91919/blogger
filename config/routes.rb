fb2cbd2509fbcca0aae73da0ff5e8438f681f628
Rando <Rando91919@gmail.com>
2015-08-16 02:20:53 -0400

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Blogger::Application.routes.draw do
   end
 
   resources :tags
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 
 end 
   # The priority is based upon order of creation: first created -> highest priority.
